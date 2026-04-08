2695b4b45c00f1f0a21a56cd0c25a8a6ba041a9e
Bartosh <jestgitbartix@gmail.com>
2026-04-08 08:32:23 +0200

    chore: remove unused files

--- Shared/Messaging/IMessageConsumer.cs
+++
@@ -1,9 +0,0 @@
-namespace Shared.Messaging;
-
-public interface IMessageConsumer : IDisposable
-{
-    Task StartConsumingAsync<T>(
-        string queue,
-        Func<T, Task> handler,
-        CancellationToken cancellationToken = default) where T : class;
-}
